8bfd7a904fc23d308ac4a35aa964322acbd330bb
GizmoTheMoonPig <cnicholasb@comcast.net>
2022-04-21 23:07:33 -0600

    removed unused horn layer

--- src/main/java/com/gizmo/luggage/client/LuggageModel.java
+++ src/main/java/com/gizmo/luggage/client/LuggageModel.java
@@ -13,13 +13,11 @@ public class LuggageModel extends HierarchicalModel<LuggageEntity> {
 
 	private final ModelPart root;
 	private final ModelPart lid;
-	public final ModelPart horns;
 	private final ModelPart[] legs = new ModelPart[21];
 
 	public LuggageModel(ModelPart root) {
 		this.root = root;
 		this.lid = root.getChild("lid");
-		this.horns = lid.getChild("horns");
 
 		for (int i = 0; i < legs.length; i++) {
 			this.legs[i] = root.getChild("leg_" + i);
@@ -30,15 +28,10 @@ public class LuggageModel extends HierarchicalModel<LuggageEntity> {
 		MeshDefinition mesh = new MeshDefinition();
 		PartDefinition partDefinition = mesh.getRoot();
 
-		var lid = partDefinition.addOrReplaceChild("lid", CubeListBuilder.create()
+		partDefinition.addOrReplaceChild("lid", CubeListBuilder.create()
 						.texOffs(0, 22)
 						.addBox(-8.0F, -1.0F, -8.0F, 16.0F, 2.0F, 8.0F, new CubeDeformation(0.01F)),
 				PartPose.offset(0.0F, 13.0F, 4.0F));
-
-		lid.addOrReplaceChild("horns", CubeListBuilder.create()
-						.texOffs(40, 0)
-						.addBox(-4.5F, -6.0F, -4.0F, 9.0F, 5.0F, 0.0F),
-				PartPose.ZERO);
 
 		partDefinition.addOrReplaceChild("base", CubeListBuilder.create()
 						.texOffs(0, 0)
@@ -64,8 +57,6 @@ public class LuggageModel extends HierarchicalModel<LuggageEntity> {
 	@Override
 	public void setupAnim(LuggageEntity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float headYaw, float headPitch) {
 		this.lid.xRot = Math.min(0, Mth.cos(limbSwing * 0.5F) * 1.4F * limbSwingAmount * 0.75F);
-
-		this.horns.visible = true;
 
 		for (int i = 0; i < legs.length; i++) {
 			int x = i % 7;
--- src/main/java/com/gizmo/luggage/client/LuggageRenderer.java
+++ src/main/java/com/gizmo/luggage/client/LuggageRenderer.java
@@ -43,7 +43,6 @@ public class LuggageRenderer extends MobRenderer<LuggageEntity, LuggageModel> {
 				this.getParentModel().prepareMobModel(luggage, limbSwing, limbSwingAmount, partialTicks);
 				VertexConsumer vertexconsumer = buffer.getBuffer(RenderType.energySwirl(new ResourceLocation("textures/entity/creeper/creeper_armor.png"), f * 0.01F % 1.0F, f * 0.01F % 1.0F));
 				this.getParentModel().setupAnim(luggage, limbSwing, limbSwingAmount, ageInTicks, headYaw, headPitch);
-				this.getParentModel().horns.visible = false;
 				this.getParentModel().renderToBuffer(ms, vertexconsumer, light, OverlayTexture.NO_OVERLAY, 0.5F, 0.5F, 0.5F, 1.0F);
 			}
 		}
