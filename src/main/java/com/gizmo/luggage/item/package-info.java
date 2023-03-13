57683f7b3d8148c194f35313b32d5a4236be875b
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2023-03-12 19:56:14 -0600

    package infos

---
+++ src/main/java/com/gizmo/luggage/item/package-info.java
@@ -0,0 +1,7 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package com.gizmo.luggage.item;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
