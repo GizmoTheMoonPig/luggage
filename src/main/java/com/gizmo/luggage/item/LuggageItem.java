975dd901d1eee8a1319e4b7e6bd34e5e65ee7a64
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2023-11-14 09:26:40 -0700

    implemented #32

--- src/main/java/com/gizmo/luggage/item/LuggageItem.java
+++ src/main/java/com/gizmo/luggage/item/LuggageItem.java
@@ -4,22 +4,31 @@ import com.gizmo.luggage.LuggageRegistries;
 import com.gizmo.luggage.client.LuggageItemRenderer;
 import com.gizmo.luggage.entity.Luggage;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.NonNullList;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.tooltip.TooltipComponent;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ItemUtils;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.items.IItemHandler;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Optional;
@@ -34,24 +43,57 @@ public class LuggageItem extends Item {
 
 	@Override
 	public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
-		HitResult result = getPlayerPOVHitResult(level, player, ClipContext.Fluid.NONE);
+		boolean insertedAny = false;
+		ItemStack stack = player.getItemInHand(hand);
+		BlockHitResult result = getPlayerPOVHitResult(level, player, ClipContext.Fluid.NONE);
 		if (result.getType() == HitResult.Type.BLOCK) {
 			Vec3 blockPos = result.getLocation();
-			if (!level.isClientSide()) {
-				Luggage luggage = LuggageRegistries.EntityRegistry.LUGGAGE.get().create(level);
-				if (luggage != null) {
-					luggage.moveTo(blockPos);
-					luggage.tame(player);
-					luggage.restoreFromStack(player.getItemInHand(hand));
-					level.addFreshEntity(luggage);
-				}
-				if (!player.getAbilities().instabuild) {
-					player.getItemInHand(hand).shrink(1);
+			//attempt to dump items into a container if shift is held and a container is targeted
+			if (!level.isClientSide() && player.isSecondaryUseActive() && level.getBlockState(BlockPos.containing(blockPos)).hasBlockEntity()) {
+				BlockEntity be = level.getBlockEntity(result.getBlockPos());
+				if (be != null && be.getCapability(ForgeCapabilities.ITEM_HANDLER, result.getDirection()).resolve().isPresent()) {
+					CompoundTag tag = stack.getTag();
+					SimpleContainer newInv = new SimpleContainer(tag != null && tag.contains(Luggage.EXTENDED_TAG) ? 54 : 27);
+					IItemHandler handler = be.getCapability(ForgeCapabilities.ITEM_HANDLER, result.getDirection()).resolve().get();
+					for (ItemStack stack1 : this.getContents(stack).toList()) {
+						for (int slot = 0; slot < handler.getSlots(); slot++) {
+							if (handler.insertItem(slot, stack1, true) != stack1) {
+								stack1 = handler.insertItem(slot, stack1, false);
+								insertedAny = true;
+								break;
+							}
+						}
+						newInv.addItem(stack1);
+					}
+					if (insertedAny && tag != null) {
+						tag.put(Luggage.INVENTORY_TAG, newInv.createTag());
+					}
 				}
 			}
-			return InteractionResultHolder.sidedSuccess(player.getItemInHand(hand), level.isClientSide());
+			if (!insertedAny) {
+				if (!level.isClientSide()) {
+					Luggage luggage = LuggageRegistries.EntityRegistry.LUGGAGE.get().create(level);
+					if (luggage != null) {
+						luggage.moveTo(blockPos);
+						luggage.tame(player);
+						luggage.restoreFromStack(stack);
+						level.addFreshEntity(luggage);
+					}
+					if (!player.getAbilities().instabuild) {
+						stack.shrink(1);
+					}
+				}
+			}
+
+			return InteractionResultHolder.sidedSuccess(stack, level.isClientSide());
+		//throw all items into the air, similar to a bundle, when holding shift and targeting no block
+		} else if (result.getType() == HitResult.Type.MISS && player.isSecondaryUseActive()) {
+			if (this.dropContents(stack, player)) {
+				player.awardStat(Stats.ITEM_USED.get(this));
+				return InteractionResultHolder.sidedSuccess(stack, level.isClientSide());
+			}
 		}
-		return InteractionResultHolder.pass(player.getItemInHand(hand));
+		return InteractionResultHolder.pass(stack);
 	}
 
 	@Override
@@ -59,7 +101,7 @@ public class LuggageItem extends Item {
 		return false;
 	}
 
-	public static Stream<ItemStack> getContentsForToolTip(ItemStack stack) {
+	private Stream<ItemStack> getContents(ItemStack stack) {
 		CompoundTag compoundtag = stack.getTag();
 		if (compoundtag == null) {
 			return Stream.empty();
@@ -72,8 +114,28 @@ public class LuggageItem extends Item {
 	@Override
 	public Optional<TooltipComponent> getTooltipImage(ItemStack stack) {
 		NonNullList<ItemStack> nonnulllist = NonNullList.create();
-		getContentsForToolTip(stack).forEach(nonnulllist::add);
+		this.getContents(stack).forEach(nonnulllist::add);
 		return nonnulllist.isEmpty() ? Optional.empty() : Optional.of(new Tooltip(nonnulllist, stack));
+	}
+
+	private boolean dropContents(ItemStack stack, Player player) {
+		CompoundTag tag = stack.getTag();
+		if (tag == null || !tag.contains(Luggage.INVENTORY_TAG)) {
+			return false;
+		} else {
+			if (player instanceof ServerPlayer) {
+				ListTag listtag = tag.getList(Luggage.INVENTORY_TAG, 10);
+
+				for(int i = 0; i < listtag.size(); ++i) {
+					CompoundTag stackTag = listtag.getCompound(i);
+					ItemStack itemstack = ItemStack.of(stackTag);
+					player.drop(itemstack, true);
+				}
+			}
+
+			stack.removeTagKey(Luggage.INVENTORY_TAG);
+			return true;
+		}
 	}
 
 	@Override
